b074fa84c9194b01e04bab5ed3ee915157555643
Wenmin Chen <wchen46@gmail.com>
2016-05-19 14:01:26 -0400

    Update header, footer, and style files and add image files

--- footer.php
+++ footer.php
@@ -4,33 +4,30 @@
  *
  * Contains the closing of the id=main div and all content after
  *
- * @package _tk
+ * @package martinlab
  */
 ?>
-			</div><!-- close .*-inner (main-content or sidebar, depending if sidebar is used) -->
-		</div><!-- close .row -->
-	</div><!-- close .container -->
-</div><!-- close .main-content -->
+  <footer id="pageFooter">
+      <h2 class="hiddenHeader">Footer</h2>
+      <div class="row">
+        <div class="col-md-1 hidden-xs hidden-sm footer-left">
+          <p class="pull-left"><a href="http://web.mit.edu/" target="_blank"><img src="<?php bloginfo('template_directory');?>/includes/img/MIT-logo.gif" alt="MIT logo" /></a></p>
+        </div>
+        <div class="col-md-8 col-sm-9 col-xs-8 footer-left">
+          <p class="img-responsive pull-left"><span class="hidden-xs"><a href="index.html"><?php bloginfo( 'name' );?></a>
+            <img src="<?php bloginfo('template_directory');?>/includes/img/sq_divider_cropped.jpg" alt="square divider" /></span> 31 Ames St, 68-459 <span class="hidden-xs"><img src="<?php bloginfo('template_directory');?>/includes/img/sq_divider_cropped.jpg" alt="square divider" /></span><span class="visible-xs-inline"><br /></span> Cambridge, MA 02139<br />
+            <span class="hidden-xs">Copyright &copy; 2010-<?php echo date( 'Y' ); ?>. All rights Reserved. <img src="<?php bloginfo('template_directory');?>/includes/img/sq_divider_cropped.jpg" alt="square divider" /> Website by Wenmin Chen</span><span class="visible-xs-block">&copy; 2010-<?php echo date( 'Y' ); ?><a href="<?php bloginfo( 'url' ); ?>"> <?php bloginfo( 'name' ); ?></a></span></p>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-4 footer-right">
+          <p class="pull-right img-responsive socialIcon"><a href="https://www.facebook.com/martinlabMIT" target="_blank" ><img src="<?php bloginfo('template_directory');?>/includes/img/facebook-32.png" alt="facebook icon" /></a>&nbsp; &nbsp;<a href="mailto:acmartin@mit.edu"><img src="<?php bloginfo('template_directory');?>/includes/img/email-32.png" alt="email icon" /></a></p>
+        </div>
+      </div>
+    </footer>
+  </div><!-- end container -->
 
-<footer id="colophon" class="site-footer" role="contentinfo">
-<?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container">
-		<div class="row">
-			<div class="site-footer-inner col-sm-12">
+  <?php get_template_part('content', 'modal'); ?>
 
-				<div class="site-info">
-					<?php do_action( '_tk_credits' ); ?>
-					<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_tk' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', '_tk' ), 'WordPress' ); ?></a>
-					<span class="sep"> | </span>
-                    <a class="credits" href="http://themekraft.com/" target="_blank" title="Themes and Plugins developed by Themekraft" alt="Themes and Plugins developed by Themekraft"><?php _e('Themes and Plugins developed by Themekraft.','_tk') ?> </a>
-				</div><!-- close .site-info -->
-
-			</div>
-		</div>
-	</div><!-- close .container -->
-</footer><!-- close #colophon -->
-
-<?php wp_footer(); ?>
-
-</body>
+  <?php wp_footer(); ?>
+   
+  </body>
 </html>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
  *
  * Displays all of the <head> section and everything up till <div id="main">
  *
- * @package _tk
+ * @package martinlab
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -13,7 +13,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<title><?php wp_title( '|', true, 'right' ); <?php bloginfo( 'name' ); ?></title>
 
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
@@ -24,72 +24,43 @@
 <body <?php body_class(); ?>>
 	<?php do_action( 'before' ); ?>
 
-<header id="masthead" class="site-header" role="banner">
-<?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container">
-		<div class="row">
-			<div class="site-header-inner col-sm-12">
+<h1 class="hiddenHeader"><?php bloginfo( 'name' ); ?></h1>
+    <header id="mainHeader">
+      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <h2 class="hiddenHeader">main navigation</h2> <!-- hide this heading from view using CSS -->
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
 
-				<?php $header_image = get_header_image();
-				if ( ! empty( $header_image ) ) { ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-					</a>
-				<?php } // end if ( ! empty( $header_image ) ) ?>
+            <?php 
+            $sitename = get_bloginfo( 'name' );
+            $pieces = explode(' ', $sitename);
+            ?>
 
+            <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><img src="<?php bloginfo('template_directory');?>/includes/img/logo.png" class="logo">
+              <span class="labName_part1"> <?php echo $pieces[0] ?></span><span class="labName_part2"><?php echo $pieces[1]; ?></span><br /><span class="tagline"><?php bloginfo( 'description' ); ?></span></a>
+          </div><!-- end navbar-header -->
+          <div class="navbar-collapse collapse">
+            
+            <?php 
+              $args = array(
+                'menu' => 'header-menu',
+                'menu_class' => 'nav navbar-nav navbar-right',
+                'container' => 'false',
+                'walker'    => new wp_bootstrap_navwalker()
+              );
+              wp_nav_menu ( $args );
+            ?>
+            
+          </div><!-- end navbar-collapse -->
+        </div><!-- end container -->
+      </nav>
+    </header>
+  <div class="container">
 
-				<div class="site-branding">
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description lead"><?php bloginfo( 'description' ); ?></p>
-				</div>
-
-			</div>
-		</div>
-	</div><!-- .container -->
-</header><!-- #masthead -->
-
-<nav class="site-navigation">
-<?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container">
-		<div class="row">
-			<div class="site-navigation-inner col-sm-12">
-				<div class="navbar navbar-default">
-					<div class="navbar-header">
-						<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-							<span class="sr-only"><?php _e('Toggle navigation','_tk') ?> </span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-	
-						<!-- Your site title as branding in the menu -->
-						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</div>
-
-					<!-- The WordPress Menu goes here -->
-					<?php wp_nav_menu(
-						array(
-							'theme_location' 	=> 'primary',
-							'depth'             => 2,
-							'container'         => 'div',
-							'container_class'   => 'collapse navbar-collapse',
-							'menu_class' 		=> 'nav navbar-nav',
-							'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
-							'menu_id'			=> 'main-menu',
-							'walker' 			=> new wp_bootstrap_navwalker()
-						)
-					); ?>
-
-				</div><!-- .navbar -->
-			</div>
-		</div>
-	</div><!-- .container -->
-</nav><!-- .site-navigation -->
-
-<div class="main-content">
-<?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container">
-		<div class="row">
-			<div id="content" class="main-content-inner col-sm-12 col-md-8">
 
