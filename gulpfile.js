2f230eff7b5da2d803da68e3bd5988a4fb772809
Wenmin Chen <wchen46@gmail.com>
2016-06-19 20:47:42 -0400

    Modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,47 @@
 var gulp = require('gulp'),
 	less = require('gulp-less'),
-  autoprefixer = require('gulp-autoprefixer')
-  minifyCSS  = require('gulp-minify-css'), 
-	rename = require('gulp-rename'); 
+  autoprefixer = require('gulp-autoprefixer');
+  //minifyCSS  = require('gulp-minify-css'), 
+	//rename = require('gulp-rename'); 
 
 /* Task to compile less */
-gulp.task('styles',function(){
+/*gulp.task('styles',function(){
 	return gulp.src('./includes/less/custom/style.less')
 		.pipe(less())
 		.pipe(autoprefixer())
 		.pipe(rename({suffix: '.expanded'}))
     .pipe(gulp.dest('./includes/css/'));
-});
+});*/
 
 /* Task to minify css */
-gulp.task('minify-css', ['styles'], function() {  
+/*gulp.task('minify-css', ['styles'], function() {  
   gulp.src('./includes/css/style.expanded.css')
     .pipe(minifyCSS())
     .pipe(rename('style.css'))
+    .pipe(gulp.dest('./'));
+});*/
+
+/* Task to watch less changes */
+//gulp.task('watch', function () {
+  //gulp.watch('./includes/less/**/*.less', ['styles']);
+  //gulp.watch('./includes/less/**/*.less', ['minify-css']);
+//});
+
+/* Task when running `gulp` from terminal */
+//gulp.task('default', ['styles', 'minify-css', 'watch']);
+
+/* Task to compile less */
+gulp.task('styles',function(){
+  return gulp.src('./includes/less/custom/style.less')
+    .pipe(less())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./'));
 });
 
 /* Task to watch less changes */
 gulp.task('watch', function () {
-  gulp.watch('./includes/less/**/*.less', ['styles']);
-  gulp.watch('./includes/less/**/*.less', ['minify-css']);
+gulp.watch('./includes/less/**/*.less', ['styles']);
 });
 
 /* Task when running `gulp` from terminal */
-gulp.task('default', ['styles', 'minify-css', 'watch']);
+gulp.task('default', ['styles', 'watch']);
